a32a6929f77b7333275684e0a563c5ab7bf7815c
samir <samalam281@gmail.com>
2023-01-27 00:50:46 +0530

    randonFiles

--- personal-portfolio/src/App.js
+++ personal-portfolio/src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-
+// import { Navigation } from './Component/Navigation';
+import ResponsiveAppBar from './Component/Navigation';
+import About from './Component/About';
+import Projects from './Component/Projects';
+import Skills from './Component/Skills';
+import Contact from './Component/contact';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <Navigation/> */}
+      <ResponsiveAppBar></ResponsiveAppBar>
+      <About/>
+      <Projects/>
+      <Skills/>
+      <Contact/>
     </div>
   );
 }
